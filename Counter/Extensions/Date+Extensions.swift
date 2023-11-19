30b157b2ffbeed0db57af6f023d1bd5bb3e03133
Andrei Zharov <zharovandrey1989@gmail.com>
2023-11-19 12:58:52 +0300

    Спринт 3. Закончил выполнение тестового задания.

--- Counter/Extensions/Date+Extensions.swift
+++ Counter/Extensions/Date+Extensions.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+private let dateAndTimeFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "[dd.MM.yyyy HH:mm:ss]: "
+    return dateFormatter
+}()
+
+extension Date {
+    var dateAndTime: String {
+        dateAndTimeFormatter.string(from: self)
+    }
+}
--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var currentValue: Int = 0 {
+        didSet {
+            counterLabel.text = "Значение счётчика: \(currentValue)"
+        }
     }
 
-
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var historyTextView: UITextView!
+    @IBOutlet weak var clearLog: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        counterLabel.text = String(currentValue)
+        historyTextView.isEditable = false
+//        historyTextView.isSelectable = false
+        historyTextView.text = "История изменений:\n"
+        historyTextView.layer.cornerRadius = 20
+        historyTextView.layer.masksToBounds = true
+        historyTextView.isScrollEnabled = true
+    }
+    
+    func printHistoryTextView (_ message: String) {
+        historyTextView.text.append(Date().dateAndTime)
+        historyTextView.text.append(message)
+    }
+    
+    @IBAction func plusCounterOnTap(_ sender: Any) {
+        printHistoryTextView("Значение изменено на +1\n")
+        currentValue += 1
+    }
+    
+    @IBAction func minusCounterOnTap(_ sender: Any) {
+        if currentValue != 0 {
+            currentValue -= 1
+            printHistoryTextView("Значение изменено на -1\n")
+        } else {
+            printHistoryTextView("Попытка уменьшить значение счётчика ниже 0\n")
+        }
+    }
+    
+    @IBAction func resetCounterOnTap(_ sender: Any) {
+        printHistoryTextView("Значение сброшено\n")
+        currentValue = 0
+    }
+    
+    @IBAction func clearTextView(_ sender: Any) {
+        historyTextView.selectAll(historyTextView)
+        if let range = historyTextView.selectedTextRange {
+            historyTextView.replace(range, withText: "")
+        }
+    }
 }
-
